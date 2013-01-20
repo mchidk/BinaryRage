200915409a635784f6fd4d7dd26d01b4f5ec7adb
Prasanth Guruprasad <nagu89@gmail.com>
2013-01-21 05:22:04 +0530

    Adding a couple of tests.

---
+++ BinaryRage.UnitTests/DBTests.cs
@@ -0,0 +1,52 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BinaryRage;
+using FizzWare.NBuilder;
+
+namespace BinaryRage.UnitTests
+{
+    public class DBTests
+    {
+        [TestFixture]
+        public class InsertTests
+        {
+            [Test]
+            public void ShouldInsertAnObjectToStore()
+            { 
+                var model = new Model{Title ="title1", ThumbUrl="http://thumb.com/title1.jpg", Description="description1", Price=5.0F};
+                BinaryRage.DB<Model>.Insert("myModel", model, "dbfile");
+
+                var result = BinaryRage.DB<Model>.Get("myModel", "dbfile");
+               
+                Assert.AreEqual(model, result);
+                BinaryRage.DB<Model>.Remove("myModel", "dbfile");
+            }
+
+            [Test]
+            public void ShouldInsertAListOfObjectsToStore()
+            {
+                var models = Builder<Model>.CreateListOfSize(3)
+                                .Build().ToList();
+                BinaryRage.DB<List<Model>>.Insert("myModels", models, "dbfile");
+
+                var result = BinaryRage.DB<List<Model>>.Get("myModels", "dbfile");
+
+                CollectionAssert.AreEqual(models, result);
+                BinaryRage.DB<List<Model>>.Remove("myModels", "dbfile");
+            }
+        }
+    }
+
+    [Serializable]
+    public class Model
+    {
+        public string Title { get; set; }
+        public string ThumbUrl { get; set; }
+        public string Description { get; set; }
+        public float Price { get; set; }
+    }
+}
