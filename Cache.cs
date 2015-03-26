8a91d556ce1c0a270b1c2170d79d21aa0d9e88c8
Hakan Lindestaf <hakan@lindestaf.com>
2015-03-26 10:34:57 -0700

    #22 Fixed formatting

--- Cache.cs
+++ Cache.cs
@@ -5,10 +5,10 @@ using System.Text;
 
 namespace BinaryRage
 {
-	internal class Cache
-	{
+    internal class Cache
+    {
         public static object LockObject = new object();
-		public static Dictionary<string, SimpleObject> CacheDic = new Dictionary<string, SimpleObject>();
-		public static int counter = 0;
-	}
+        public static Dictionary<string, SimpleObject> CacheDic = new Dictionary<string, SimpleObject>();
+        public static int counter = 0;
+    }
 }
