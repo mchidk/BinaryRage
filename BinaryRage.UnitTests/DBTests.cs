2b5cb801d8ff7296f02d9e6772c42ee4c1f502d9
Ray <rsuelzer@ufcw.org>
2015-07-09 14:23:58 -0400

    Fix broken tests.

--- BinaryRage.UnitTests/DBTests.cs
+++ BinaryRage.UnitTests/DBTests.cs
@@ -16,14 +16,14 @@ namespace BinaryRage.UnitTests
         {
             [Test]
             public void ShouldInsertAnObjectToStore()
-            { 
-                var model = new Model{Title ="title1", ThumbUrl="http://thumb.com/title1.jpg", Description="description1", Price=5.0F};
-                BinaryRage.DB<Model>.Insert("myModel", model, "dbfile");
+            {
+                var model = new Model { Title = "title1", ThumbUrl = "http://thumb.com/title1.jpg", Description = "description1", Price = 5.0F };
+                BinaryRage.DB.Insert("myModel", model, "dbfile");
 
-                var result = BinaryRage.DB<Model>.Get("myModel", "dbfile");
-               
+                var result = BinaryRage.DB.Get<Model>("myModel", "dbfile");
+
                 Assert.AreEqual(model, result);
-                BinaryRage.DB<Model>.Remove("myModel", "dbfile");
+                BinaryRage.DB.Remove("myModel", "dbfile");
             }
 
             [Test]
@@ -31,12 +31,12 @@ namespace BinaryRage.UnitTests
             {
                 var models = Builder<Model>.CreateListOfSize(3)
                                 .Build().ToList();
-                BinaryRage.DB<List<Model>>.Insert("myModels", models, "dbfile");
+                BinaryRage.DB.Insert("myModels", models, "dbfile");
 
-                var result = BinaryRage.DB<List<Model>>.Get("myModels", "dbfile");
+                var result = BinaryRage.DB.Get<List<Model>>("myModels", "dbfile");
 
                 CollectionAssert.AreEqual(models, result);
-                BinaryRage.DB<List<Model>>.Remove("myModels", "dbfile");
+                BinaryRage.DB.Remove("myModels", "dbfile");
             }
         }
     }
