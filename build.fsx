bd16287b70abb9abfd50b54cce877da9d46f339e
Jason Kanaris <phantomtypist@hotmail.com>
2015-10-26 11:50:06 -0400

    Added FAKE build script
    - This included the need for a local nuget.exe
    - Updated git ignore file to ignore FAKE related stuff.

---
+++ build.fsx
@@ -0,0 +1,85 @@
+// include Fake lib
+#r @"packages/FAKE/tools/FakeLib.dll"
+
+open System
+open System.IO
+open Fake
+
+RestorePackages()
+
+// Directories
+let buildDir  = @".\build\"
+let testDir   = @".\test\"
+let deployDir = @".\deploy\"
+let packagesDir = @".\packages\"
+
+// Properties
+let buildMode = getBuildParamOrDefault "buildMode" "Release"
+let buildNumber = getBuildParamOrDefault "buildNumber" "0"
+let releaseNotes = 
+    ReadFile "ReleaseNotes.md"
+    |> ReleaseNotesHelper.parseReleaseNotes
+let projectName = "BinaryRage"
+let version = releaseNotes.AssemblyVersion + "." + buildNumber
+
+// Targets
+Target "Clean" (fun _ ->
+    trace " --- Cleaning directories --- "
+
+    CleanDirs [buildDir; testDir; deployDir]
+)
+
+open Fake.AssemblyInfoFile
+
+Target "SetAssemblyInfo" (fun _ ->
+    trace " --- Setting assembly version information --- "
+
+    CreateCSharpAssemblyInfo "./Properties/AssemblyVersionInfo.cs"
+        [Attribute.Title projectName
+         Attribute.Description projectName
+         Attribute.Guid "191D893F-7087-4E59-9AEE-5FEF4172E3FC"
+         Attribute.Product projectName
+         Attribute.Version version
+         Attribute.FileVersion version]
+)
+
+Target "BuildBinaryRage" (fun _ ->
+    trace " --- Building BinaryRage --- "
+
+    !! @".\BinaryRage.csproj"
+        |> MSBuildRelease buildDir "Build"
+        |> Log "AppBuild-Output: "
+
+    trace " --- Fininshed building BinaryRage --- "
+)
+
+Target "BuildTests" (fun _ ->
+    !! @".\BinaryRage.UnitTests\BinaryRage.FunctionalTests.csproj"
+        |> MSBuildDebug testDir "Build"
+        |> Log "TestBuild-Output: "
+)
+
+Target "RunTests" (fun _ ->
+    !! (testDir + @"\BinaryRage.UnitTests.dll")
+      |> NUnit (fun p ->
+                 {p with
+                   DisableShadowCopy = true;
+                   OutputFile = testDir + @"TestResults.xml"})
+)
+
+Target "Deploy" (fun _ ->
+    CopyDir deployDir buildDir allFiles
+)
+
+Target "Default" DoNothing
+
+// Dependencies
+"Clean" 
+  ==> "BuildBinaryRage"
+  ==> "BuildTests"
+  ==> "RunTests"
+  ==> "Deploy"
+  ==> "Default"
+
+// start build
+RunTargetOrDefault "Default"
